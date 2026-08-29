0a602dde9fd2057e704a68e591c372927c53926e
Mark Sivolap <mark.sivolap@outlook.com>
2026-08-29 20:12:06 +0100

    second commit

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(_req: NextRequest) {
+export async function POST() {
   return NextResponse.json({
     message: "Hello from /api/example",
   });
